fed3d92185870731c8ff487073d11b3b41bd0a98
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2022-11-21 01:51:37 +0100

    Improve code quality

--- projects/Haru/Server/Servers.cs/NotificationServer.cs
+++ projects/Haru/Server/Servers.cs/NotificationServer.cs
@@ -10,31 +10,35 @@ namespace Haru.Server
         public NotificationServer()
         {
             Server = new WebSocketServer.Server("ws://127.0.0.1:80/");
-
-            Server.OnClientConnected += (object sender, OnClientConnectedEvent e) => 
-            {
-                Log.Write($"Client with GUID: {e.Client.Guid} Connected!");
-            };
-
-            Server.OnClientDisconnected += (object sender, OnClientDisconnectedEvent e) =>
-            {
-                Log.Write($"Client {e.Client.Guid} Disconnected");
-            };
-
-            Server.OnMessageReceived += (object sender, OnMessageReceivedEvent e) =>
-            {
-                Log.Write($"Received Message: 'e.Client.Guid' from client: {e.Message}");
-            };
-
-            Server.OnSendMessage += (object sender, OnSendMessageEvent e) =>
-            {
-                Log.Write($"Sent message: '{e.Message}' to client {e.Client.Guid}");
-            };
+            Server.OnClientConnected += OnClientConnected;
+            Server.OnClientDisconnected += OnClientDisconnected;
+            Server.OnMessageReceived += OnMessageReceived;
+            Server.OnSendMessage += OnSendMessage;
         }
 
         public void Start()
         {
             Server.Start();
+        }
+
+        private void OnClientConnected(object sender, OnClientConnectedEvent e)
+        {
+            Log.Write($"Client with GUID: {e.Client.Guid} Connected!");
+        }
+
+        private void OnClientDisconnected(object sender, OnClientDisconnectedEvent e)
+        {
+            Log.Write($"Client {e.Client.Guid} Disconnected");
+        }
+
+        private void OnMessageReceived(object sender, OnMessageReceivedEvent e)
+        {
+            Log.Write($"Received Message: 'e.Client.Guid' from client: {e.Message}");
+        }
+
+        private void OnSendMessage(object sender, OnSendMessageEvent e)
+        {
+            Log.Write($"Sent message: '{e.Message}' to client {e.Client.Guid}");
         }
     }
 }
