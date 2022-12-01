b7f1471488720a7fc0fea91d21879180ab3ea35c
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2022-12-01 19:30:26 +0100

    Improve code quality (tnx Waffle.Lord!)

--- projects/Haru/Utils/Zlib.cs
+++ projects/Haru/Utils/Zlib.cs
@@ -7,42 +7,34 @@ namespace Haru.Utils
 {
     public class Zlib
     {
+        private async Task<byte[]> Run(byte[] data, bool compress, ZlibCompression level = ZlibCompression.Store)
+        {
+            using (var msOut = new MemoryStream())
+            {
+                // ZOutputStream.Close() flushes itself.
+                // ZOutputStream.Flush() flushes the target stream.
+                // It's fucking stupid, but whatever.
+                // -- Waffle.Lord, 2022-12-01
+
+                using (var zsOut = (compress)
+                    ? new ZOutputStream(msOut, (int)level)
+                    : new ZOutputStream(msOut))
+                {
+                    await zsOut.WriteAsync(data, 0, data.Length);
+                }
+
+                return msOut.ToArray();
+            }
+        }
+
         public async Task<byte[]> Compress(byte[] data, ZlibCompression level)
         {
-            var buffer = new byte[data.Length + 24];
-            var zs = new ZStream()
-            {
-                avail_in = data.Length,
-                next_in = data,
-                next_in_index = 0,
-                avail_out = buffer.Length,
-                next_out = buffer,
-                next_out_index = 0
-            };
-
-            zs.deflateInit((int)level);
-            zs.deflate(zlibConst.Z_FINISH);
-
-            using (var ms = new MemoryStream())
-            {
-                await ms.WriteAsync(zs.next_out, 0, zs.next_out_index);
-                return ms.ToArray();
-            }
+            return await Run(data, true, level);
         }
 
         public async Task<byte[]> Decompress(byte[] data)
         {
-            using (var msOut = new MemoryStream())
-            {
-                using (var zsOut = new ZOutputStream(msOut))
-                {
-                    using (var msIn = new MemoryStream(data))
-                    {
-                        await msIn.CopyToAsync(zsOut);
-                        return msOut.ToArray();
-                    }
-                }
-            }
+            return await Run(data, false);
         }
     }
 }
