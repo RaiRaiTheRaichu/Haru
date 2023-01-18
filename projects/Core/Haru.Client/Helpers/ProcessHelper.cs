52eca33cd3cb20e63853155bfa09e484add2db49
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2023-01-18 13:04:45 +0100

    Fix application not closing

--- projects/Core/Haru.Client/Helpers/ProcessHelper.cs
+++ projects/Core/Haru.Client/Helpers/ProcessHelper.cs
@@ -8,20 +8,17 @@ namespace Haru.Client.Helpers
     public class ProcessHelper : MonoBehaviour
     {
         private const string _processPath = "EscapeFromTarkov_Data/Managed/Haru.Server.exe";
-        private readonly ProcessStartInfo _processStartInfo;
         private readonly Process _process;
 
         public ProcessHelper()
         {
-            var processStartInfo = new ProcessStartInfo()
-            {
-                FileName = Path.Combine(Environment.CurrentDirectory, _processPath),
-                WorkingDirectory = Environment.CurrentDirectory
-            };
-
             _process = new Process()
             {
-                StartInfo = processStartInfo
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = Path.Combine(Environment.CurrentDirectory, _processPath),
+                    WorkingDirectory = Environment.CurrentDirectory
+                }
             };
         }
 
@@ -32,7 +29,8 @@ namespace Haru.Client.Helpers
 
         private void OnApplicationQuit()
         {
-            _process.Close();
+            _process.CloseMainWindow();
+            _process.Dispose();
         }
     }
 }
